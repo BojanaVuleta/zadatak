bd70b26d13e7614f1f7f768b25c62127fd8e6eb0
BojanaVuleta <vuleta.bojana8@gmail.com>
2023-03-01 15:51:23 +0100

    Third commit

--- comments.php
+++ comments.php
@@ -1,20 +1,23 @@
 <?php  
 include "db.php";
 
-    $id = $_GET['id'];
-    $sql2 = "SELECT * FROM comments WHERE post_id = $id";
+    $id = $_GET['post_id'];
+    $sql2 = "SELECT * FROM comments WHERE posts_id = {$id}";
     $statement = $connection->prepare($sql2);
     $statement->execute();
     $statement->setFetchMode(PDO::FETCH_ASSOC);
     $comments = $statement->fetchAll();
 ?>
+<ul>
 <?php 
     foreach ($comments as $comment) { ?>
          <li>
             <hr>
+            
                 <p>Comment by: <?php echo $comment['author']; ?></a></p>
                 <p><?php echo $comment['text'] ?></p>
             <hr>
         </li>
  <?php } ?>
+</ul>
  
